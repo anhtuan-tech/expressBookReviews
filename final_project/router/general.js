bce22f51992f3dbfa72aae5d304b6aa923bfd299
Le Anh Tuan <tuanlace180905@fpt.edu.vn>
2026-08-13 08:50:23 +0700

    hehe

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -3,41 +3,48 @@ let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
+const axios = require('axios'); // Thêm dòng này
 
-
+// Đăng ký user
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+  const password = req.body.password;
+  if (username && password) {
+      users.push({"username": username, "password": password});
+      return res.status(200).json({message: "Customer successfully registered. Now you can login."});
+  }
+  return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
+// Lấy tất cả sách
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).send(JSON.stringify(books, null, 4));
 });
 
-// Get book details based on ISBN
+// Lấy sách theo ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
-// Get book details based on author
+  const isbn = req.params.isbn;
+  return res.status(200).json(books[isbn]);
+});
+
+// Lấy sách theo Author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  let filteredBooks = Object.values(books).filter(book => book.author === author);
+  return res.status(200).json(filteredBooks);
 });
 
-// Get all books based on title
+// Lấy sách theo Title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  let filteredBooks = Object.values(books).filter(book => book.title === title);
+  return res.status(200).json(filteredBooks);
 });
 
-//  Get book review
+// Lấy review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.status(200).json(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
